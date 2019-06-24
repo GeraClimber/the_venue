ad20224a42abd9c1ef8adcba50520d8ff7383729
lazy.lizard@yandex.ru <NEE6qmVHW7QWKY38a47Q>
2019-06-25 01:07:08 +0300

    * Renames * relocation * proper exports and imports

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,48 +1,45 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import MenuIcon from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
 
-import SideDrawer from './SideDrawer'
+import {SideDrawer} from '../side-drawer'
 
-export default class Header extends Component {
-
+export class Header extends Component {
 
     state = {
         right: false,
         headerShow: false
-      }
-    
-
+    };
 
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll)
     }
 
     handleScroll = () => {
-        if(window.scrollY>0){
+        if (window.scrollY > 0) {
             this.setState({
-                headerShow:true
+                headerShow: true
             })
-        }else{
+        } else {
             this.setState({
-                headerShow:false
+                headerShow: false
             })
         }
-    }
+    };
 
     toggleDrawer = (side, open) => () => {
         this.setState({
-          [side]: open,
+            [side]: open,
         });
-    }
+    };
 
     render() {
         return (
             <AppBar
-                position = "fixed"
-                style = {{
+                position="fixed"
+                style={{
                     backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                     boxShadow: 'none',
                     padding: '10px 0px'
@@ -58,13 +55,13 @@ export default class Header extends Component {
                         color="inherit"
                         onClick={this.toggleDrawer('right', true)}
                     >
-                        
-                        <MenuIcon/>
+
+                        <MenuIcon />
                     </IconButton>
 
                     <SideDrawer
                         open={this.state.right}
-                        onClose={(value)=> this.toggleDrawer("right", value)}
+                        onClose={(value) => this.toggleDrawer("right", value)}
                     />
                 </Toolbar>
             </AppBar>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {Header} from './header';
+export {Footer} from './footer';
+export {SideDrawer} from './side-drawer';
+export {Location} from './location';
+export {Pricing} from './pricing';
