42bfb79ad55afaa1d9481611a2ea2aa14255e349
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-07-01 08:50:51 +0300

    add header2

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,7 +6,7 @@ import IconButton from '@material-ui/core/IconButton';
 
 import SideDrawer from './SideDrawer';
 
-class Header extends Component {
+export class Header extends Component {
 
 
     state = {
